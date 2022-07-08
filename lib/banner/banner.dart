5616919f0ab53a689067a483fe0782b870f019aa
Rizal Fauzan <rizalfauzan190@gmail.com>
2022-07-08 21:40:38 +0700

    [update sizeScreen]

--- lib/banner/banner.dart
+++ lib/banner/banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:protfolio_app/utilities/theme/theme.dart';
 
 class BannerPage extends StatefulWidget {
   const BannerPage({Key? key}) : super(key: key);
@@ -11,8 +12,8 @@ class _BannerPage extends State<BannerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('banner'),
-    ));
+      backgroundColor: bColor2,
+      body: Container(),
+    );
   }
 }
