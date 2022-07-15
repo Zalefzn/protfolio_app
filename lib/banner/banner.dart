b70ca25dd11fd52dd50c6d840cb4c588887470c1
Rizal Fauzan <rizalfauzan190@gmail.com>
2022-07-15 21:14:08 +0700

    [update homePage]]

--- lib/banner/banner.dart
+++ lib/banner/banner.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:protfolio_app/homePage/homePage.dart';
+import 'package:protfolio_app/utilities/screenSize/sizeScreen.dart';
 import 'package:protfolio_app/utilities/theme/theme.dart';
 
 class BannerPage extends StatefulWidget {
@@ -9,11 +11,75 @@ class BannerPage extends StatefulWidget {
 }
 
 class _BannerPage extends State<BannerPage> {
+  bool loading = false;
+
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        loading = true;
+      });
+
+      return loading = false;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       backgroundColor: bColor2,
-      body: Container(),
+      body: Stack(children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  opacity: 0.5,
+                  image: AssetImage('assets/post10.png'))),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: SizeConfig.blockVertical * 75),
+          padding: EdgeInsets.only(left: SizeConfig.blockHorizontal * 17),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'welcome,\nthis is my portfolio as a designer',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: textColor1,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: SizeConfig.blockVertical * 4),
+              Container(
+                height: SizeConfig.blockVertical * 6.5,
+                width: SizeConfig.blockHorizontal * 70,
+                decoration: BoxDecoration(
+                    color: buttonColor,
+                    borderRadius: BorderRadius.circular(30)),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        loading
+                            ? const CircularProgressIndicator()
+                            : Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()),
+                                (route) => false);
+                      });
+                    },
+                    child: Text('Next',
+                        style: TextStyle(
+                            color: textColor1,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w800))),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
--- lib/homePage/homePage.dart
+++ lib/homePage/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:protfolio_app/utilities/screenSize/sizeScreen.dart';
+import 'package:protfolio_app/utilities/theme/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +11,82 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SizeConfig().init(context);
+
+    Widget contentBottom() {
+      return Container(
+          margin: EdgeInsets.only(
+              top: SizeConfig.blockVertical * 80,
+              left: SizeConfig.blockHorizontal * 5),
+          height: SizeConfig.blockVertical * 15,
+          width: SizeConfig.blockHorizontal * 90,
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: SizeConfig.blockHorizontal * 2),
+                height: SizeConfig.blockVertical * 13,
+                width: SizeConfig.blockHorizontal * 25,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage('assets/pict.jpg'),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              SizedBox(
+                width: SizeConfig.blockHorizontal * 4,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Victor de Paula',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w800)),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text('Read More',
+                        style: TextStyle(
+                            color: Colors.amber[400],
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                ],
+              )
+            ],
+          ));
+    }
+
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            Container(
+              height: SizeConfig.blockVertical * 100,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      opacity: 0.5,
+                      image: AssetImage('assets/pict.jpg'),
+                      fit: BoxFit.cover)),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: SizeConfig.blockVertical * 5,
+                    left: SizeConfig.blockHorizontal * 5),
+                height: SizeConfig.blockVertical * 6,
+                width: SizeConfig.blockHorizontal * 15,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/youtube.png')),
+                ),
+              ),
+            ),
+            contentBottom()
+          ],
+        ));
   }
 }
